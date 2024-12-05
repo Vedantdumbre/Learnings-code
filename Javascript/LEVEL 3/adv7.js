b16b72e8d387f6dc932e7049171942b68a6518f2
Vedant <vedantdumbre0706@gmail.com>
2024-12-06 00:45:56 +0530

    level 3 starting

---
+++ Javascript/LEVEL 3/adv7.js
@@ -0,0 +1,18 @@
+//DOM(Document object manipulation) 
+
+//DOM MANIPULATION : html ke kisibhi element ko change kar pana as per our need 
+
+// 1.Slection:
+var home = document.querySelector("#home"); //here home is the place where we will srore our html tag ;here ive used a 'id' to select the home element
+
+// 2. Changing CSS:
+home.style.color = "red";
+home.style.backgroundColor = "yellow"; // while doing dom manipulation we must always have a practice of "camelCase" writing.
+home.style.fontFamily = "poppins"; 
+
+// 3. Changing HTML
+home.innerHTML = "<i>hey kiase ho</i>"; // yahape hume "innerhtml" isiliye use karna hai kyuki hum cahtehai ki hume kisi selected html element ke andar kuch bhi likhneka chance mile;IN THIS CASE: hume "hey kaise ho" likha hua milega website me 
+
+// 4. text change :
+home.textContent = "<I>hey kiase ho</I>"; // main diff innerhtml aur textcontent ka hai ki wo dono bhi html ke tags me jake kuch bhi likh sakte hai but; textchange me hum jo bhi likhenge wo as it is hume page pe dikhayi denelagega ;IN THIS CASE: hume yahape <I>hey kiase ho</I> likha hhua milega 
+
