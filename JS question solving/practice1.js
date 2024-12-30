3d78ff085792508dadde747a1a2f9ec12a0e84f1
Vedant <vedantdumbre0706@gmail.com>
2024-12-30 23:20:32 +0530

    solved 2 array realted problems

---
+++ JS question solving/practice1.js
@@ -0,0 +1,17 @@
+// find the average if the following numbers (ARRAY):
+
+let marks = [42,32,56,78,99,86]
+
+sum =0;
+for (i=0; i<marks.length; i++){
+    sum += marks[i];
+}
+console.log(sum/marks.length);
+
+// find the 10 % value of the same array:
+
+let tenPercent = marks.length-0.1;
+console.log(tenPercent);
+
+
+// find the median:
