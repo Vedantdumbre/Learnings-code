cf7c1804d726ab1159fa3e4c1baaaeb1a89af70d
Vedant <vedantdumbre0706@gmail.com>
2025-01-06 23:26:28 +0530

    types of map methods

--- Javascript/LEVEL 3/adv8.js
+++ Javascript/LEVEL 3/adv8.js
@@ -2,3 +2,25 @@
 
 // diff btwn MAPS & OBJECTS :
 //->
+
+
+//types for creating maps:
+//1.New Map() :
+/*const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);*/
+
+//2.set( ):
+/*// Create a Map
+const fruits = new Map();
+
+// Set Map Values
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);*/
+
+
+//get ():
+// fruits.get("apples");  
