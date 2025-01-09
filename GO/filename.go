3b155725dd7aaaa5ec7b26d216ffee143cb96434
Vedant <vedantdumbre0706@gmail.com>
2025-01-09 23:23:39 +0530

    managing files

--- GO/filename.go
+++ GO/filename.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
-    fmt.Println("Welcome to Go!")
+func main() {    
+    fmt.Println("Welcome to Go!") // this is used to print
 }
 //this is a sample code for go language
