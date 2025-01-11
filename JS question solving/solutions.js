e7253e97f04251cc2138b75e88a3bf6f5cc68ac1
Vedant <vedantdumbre0706@gmail.com>
2025-01-11 23:25:03 +0530

    loop strat

---
+++ JS question solving/solutions.js
@@ -0,0 +1,32 @@
+//Q1. Create a for loop that prints number 1 to 10.
+
+for(let  i= 
+1
+; i<=
+10
+; i++){
+    console.log(
+i
+);
+}
+
+//Q2. Create a while loop that runs when i is less than 7
+
+let i=0; 
+while(
+i<7
+){ 
+    console.log(i); 
+    i++; 
+}
+
+//Q3. Make this loop skip the iteration when k = 4
+
+for( let k=0 ; k<5; k++){ 
+    if(k==4){
+        
+continue
+;
+    }
+    console.log(k); 
+} 
