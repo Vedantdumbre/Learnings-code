2bd647a761fc60b1d61538dcbe87cbd9362ebac5
Vedant <vedantdumbre0706@gmail.com>
2025-01-13 23:13:29 +0530

    maps

--- Javascript/LEVEL 3/adv8.js
+++ Javascript/LEVEL 3/adv8.js
@@ -13,13 +13,13 @@
   ]);*/
 
 //2.set( ):
-/*// Create a Map
+// Create a Map
 const fruits = new Map();
 
 // Set Map Values
 fruits.set("apples", 500);
 fruits.set("bananas", 300);
-fruits.set("oranges", 200);*/
+fruits.set("oranges", 200);
 
 
 //get ():
