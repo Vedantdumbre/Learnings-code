887884cccfb0a65a5367b7f9b7b5f144e549c024
Vedant <vedantdumbre0706@gmail.com>
2025-02-04 22:54:01 +0530

    event handling

--- Javascript/LEVEL 3/Event handling/3_Events.js
+++ Javascript/LEVEL 3/Event handling/3_Events.js
@@ -2,5 +2,5 @@
 let button = document.getElementById("btn");
 
 button.addEventListener("click",()=>{
-    document.querySelector("h1").innerHTML = "<b> yes i was clicked </b>";
+    alert("i was clicked!")
 })
