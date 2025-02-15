42468739a89843d298a4f1b150558f2f643ca8c5
Vedant <vedantdumbre0706@gmail.com>
2025-02-15 21:10:10 +0530

    procceding in callback and promises

--- Javascript/LEVEL 3/Callback & Promises/script.js
+++ Javascript/LEVEL 3/Callback & Promises/script.js
@@ -4,6 +4,6 @@ console.log("hello bro2")
 
 setTimeout( ()=>{
     console.log("settimeout ");
-},2000);
+},2000); //yaha pe jo bhi likhoge wo last me hi print hojayega; bcoz yhape script.js hamehsa baki code ko pehle run karne dega then wo is code ke pass dekhega . Even if u type "0" as time .
 
 console.log("hello bro3");
