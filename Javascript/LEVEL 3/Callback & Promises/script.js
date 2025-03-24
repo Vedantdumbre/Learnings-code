6164bcb073f452db7f7b786eac544084b999e9c5
Vedant <vedantdumbre0706@gmail.com>
2025-03-24 22:59:34 +0530

    callabacks

--- Javascript/LEVEL 3/Callback & Promises/script.js
+++ Javascript/LEVEL 3/Callback & Promises/script.js
@@ -3,7 +3,7 @@ console.log("hello bro2")
 
 
 setTimeout( ()=>{
-    console.log("settimeout ");
-},2000); ///yaha pe jo bhi likhoge wo last me hi print hojayega; bcoz yhape script.js hamehsa baki code ko pehle run karne dega then wo is code ke pass dekhega . Even if u type "0" as time .
+    console.log("settimeout (after 3 seconds)");
+},3000); ///yaha pe jo bhi likhoge wo last me hi print hojayega; bcoz yhape script.js hamehsa baki code ko pehle run karne dega then wo is code ke pass dekhega . Even if u type "0" as time .
 
 console.log("hello bro3");
